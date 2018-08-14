0eb7e7a9fae6dcfe99d5544022f2b8db558f26bf
CHenVenus <310686064@qq.com>
2018-08-14 23:18:26 +0800

    html5新语义标签运用

---
+++ html5Tag/js/dropdown.js
@@ -0,0 +1,8 @@
+function over(li){
+	var dropdownmenu=li.getElementsByTagName("ul")[0];
+	dropdownmenu.style.display="block";
+}
+function out(li){
+	var dropdownmenu=li.getElementsByTagName("ul")[0];
+	dropdownmenu.style.display="none";
+}
